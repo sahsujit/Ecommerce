f487c8fa4b8cddd5091957aa9d163bb77c7eeb06
sujit sah <sujit8877123sah@gmail.com>
2024-09-07 10:07:39 +0545

    debug

--- src/services/product/index.js
+++ src/services/product/index.js
@@ -1,122 +1,10 @@
-// //add a new product service
-
-// import Cookies from "js-cookie";
-
-// export const addNewProduct = async (formData) => {
-//   try {
-//     const response = await fetch("/api/admin/add-product", {
-//       method: "POST",
-//       headers: {
-//         "content-type": "application/json",
-//         Authorization: `Bearer ${Cookies.get("token")}`,
-//       },
-//       body: JSON.stringify(formData),
-//     });
-
-//     const data = await response.json();
-
-//     return data;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// export const getAllAdminProducts = async () => {
-//   try {
-//     const res = await fetch("http://localhost:3000/api/admin/all-product", {
-//       method: "GET",
-//       cache: "no-store",
-//     });
-
-//     const data = await res.json();
-
-//     return data;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// export const updateAProduct = async (formData) => {
-//   try {
-//     const res = await fetch("/api/admin/update-product", {
-//       method: "PUT",
-//       headers: {
-//         "content-type": "application/json",
-//         Authorization: `Bearer ${Cookies.get("token")}`,
-//       },
-//       cache: "no-store",
-//       body: JSON.stringify(formData),
-//     });
-
-//     const data = await res.json();
-
-//     return data;
-//   } catch (e) {
-//     console.log(e);
-//   }
-// };
-
-// export const deleteAProduct = async (id) => {
-//   try {
-//     const res = await fetch(`/api/admin/delete-product?id=${id}`, {
-//       method: "DELETE",
-//       headers: {
-//         Authorization: `Bearer ${Cookies.get("token")}`,
-//       },
-//     });
-
-//     const data = await res.json();
-
-//     return data;
-//   } catch (e) {
-//     console.log(e);
-//   }
-// };
-
-// export const productByCategory = async (id) => {
-//   try {
-//     const res = await fetch(
-//       `http://localhost:3000/api/admin/product-by-category?id=${id}`,
-//       {
-//         method: "GET",
-//         cache: "no-store",
-//       }
-//     );
-
-//     const data = await res.json();
-
-//     return data;
-//   } catch (e) {
-//     console.log(e);
-//   }
-// };
-
-// export const productById = async (id) => {
-//   try {
-//     const res = await fetch(
-//       `http://localhost:3000/api/admin/product-by-id?id=${id}`,
-//       {
-//         method: "GET",
-//         cache: "no-store",
-//       }
-//     );
-
-//     const data = await res.json();
-
-//     return data;
-//   } catch (e) {
-//     console.log(e);
-//   }
-// };
-
+//add a new product service
 
 import Cookies from "js-cookie";
 
-// const baseURL = ""; // Use an empty string for relative URLs
-
 export const addNewProduct = async (formData) => {
   try {
-    const response = await fetch(`/api/admin/add-product`, {
+    const response = await fetch("/api/admin/add-product", {
       method: "POST",
       headers: {
         "content-type": "application/json",
@@ -126,6 +14,7 @@ export const addNewProduct = async (formData) => {
     });
 
     const data = await response.json();
+
     return data;
   } catch (error) {
     console.log(error);
@@ -134,13 +23,13 @@ export const addNewProduct = async (formData) => {
 
 export const getAllAdminProducts = async () => {
   try {
-    const url = `${process.env.BASE_URL}/api/admin/all-product`;
-    console.log(`Fetching URL: ${url}`);
-    const res = await fetch(url, {
+    const res = await fetch(`${process.env.BASE_URL}/api/admin/all-product`, {
       method: "GET",
+      cache: "no-store",
     });
 
     const data = await res.json();
+
     return data;
   } catch (error) {
     console.log(error);
@@ -149,16 +38,18 @@ export const getAllAdminProducts = async () => {
 
 export const updateAProduct = async (formData) => {
   try {
-    const res = await fetch(`/api/admin/update-product`, {
+    const res = await fetch("/api/admin/update-product", {
       method: "PUT",
       headers: {
         "content-type": "application/json",
         Authorization: `Bearer ${Cookies.get("token")}`,
       },
+      cache: "no-store",
       body: JSON.stringify(formData),
     });
 
     const data = await res.json();
+
     return data;
   } catch (e) {
     console.log(e);
@@ -175,6 +66,7 @@ export const deleteAProduct = async (id) => {
     });
 
     const data = await res.json();
+
     return data;
   } catch (e) {
     console.log(e);
@@ -183,11 +75,16 @@ export const deleteAProduct = async (id) => {
 
 export const productByCategory = async (id) => {
   try {
-    const res = await fetch(`${process.env.BASE_URL}/api/admin/product-by-category?id=${id}`, {
-      method: "GET",
-    });
+    const res = await fetch(
+      `${process.env.BASE_URL}/api/admin/product-by-category?id=${id}`,
+      {
+        method: "GET",
+        cache: "no-store",
+      }
+    );
 
     const data = await res.json();
+
     return data;
   } catch (e) {
     console.log(e);
@@ -196,11 +93,16 @@ export const productByCategory = async (id) => {
 
 export const productById = async (id) => {
   try {
-    const res = await fetch(`${process.env.BASE_URL}/api/admin/product-by-id?id=${id}`, {
-      method: "GET",
-    });
+    const res = await fetch(
+      `${process.env.BASE_URL}/api/admin/product-by-id?id=${id}`,
+      {
+        method: "GET",
+        cache: "no-store",
+      }
+    );
 
     const data = await res.json();
+
     return data;
   } catch (e) {
     console.log(e);
